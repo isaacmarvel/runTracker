64c449da7bac1cb458e406cf9da4bf5bdedeb31f
Isaac Marvel <isaac.marvel.77@gmail.com>
2022-06-23 12:54:35 -0400

    Added example API with cats

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-let express = require("express");
-let app = express(); //creates an express application
-let router = express.Router();
-
-router.get("/", function(req, res, next) { //next is for middleware error handling
-    res.send("Apple"); //whatever I want to be sent. Probably want this to send form data
-});
-
-app.use("/api/", router); //this will add /api/ onto the web path
-
-var server = app.listen(5000, function () {
-    console.log("Node server is running on http://localhost:5000..");
-});
-
 function openMenu() {
     let x = document.getElementById("myLinks");
      if (x.style.display === "block") {
@@ -20,3 +6,7 @@ function openMenu() {
       x.style.display = "block";
     }
 } 
+
+//function validateForm() {
+
+//}
