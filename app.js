d0276712849214f7a3f3b2bcd53b43cb97b65de7
Isaac <isaac.marvel.77@gmail.com>
2022-07-13 11:25:53 -0400

    Added in api and server code, not currently working

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const sqlite = require('better-sqlite3');
+const app = express();
+const port = 3000;
+app.use(express.static('public')); //sets up middleware to send file automatically if it matches file in public directory
+app.use(express.json()); //parses json into an object
+
+function getInfo(db) { //research what these mean
+    const stmt = db.prepare("SELECT date, speed, time FROM runInfo");
+    const infoFromDb = stmt.all();
+    return infoFromDb;
+}; 
+
+function initDb() { //creates the database table if it doesn't already exist
+    let newString = `CREATE TABLE IF NOT EXISTS "runInfo" (
+        "date"  TEXT NOT NULL, //should this be a date instead of an integer?
+        "speed"    INTEGER NOT NULL,
+        "time"    INTEGER NOT NULL,
+        "id"    INTEGER,
+        PRIMARY KEY("id")
+    );`
+    let db = sqlite("runInfo.db");
+    db.prepare(newString)
+    .run();
+}; 
+
+initDb();
+
+app.get('/runInfo', (req, res) => {
+    let db = sqlite("runInfo.db")
+    let runInfoFromDb = getInfo(db);
+    res.send(JSON.stringify(runInfoFromDb))
+}); 
+
+app.post('/runInfo', (req, res) => {
+    let db = sqlite("runInfo.db")
+    const stmt = db.prepare("insert into runInfo (date, speed, time) values (?, ?, ?);");
+    const info = stmt.run(req.body.date, req.body.speed, req.body.time);
+    let runInfoFromDb = getInfo(db);
+    res.send(JSON.stringify(runInfoFromDb))
+}); 
+
+app.listen(port, () => {
+    console.log(`app listening on port ${port}`)
+});
--- public/client.js
+++ public/client.js
@@ -10,3 +10,36 @@ function openMenu() {
 //function validateForm() {
 
 //}
+
+fetch('/runInfo') 
+  .then(response => response.json())
+  .then(loadInfo);
+
+function loadInfo(info) {
+  let infoElements = "";
+  for (let info of runInfo) {
+    infoElements += `<li>${info.date} - ${info.speed} - ${info.time} </li>`
+  };
+  let ul = document.getElementById("infoList");
+  ul.innerHTML = infoElements;
+  console.log(infoElements);
+};
+
+function createNewInfo() {
+  let runSpeed = document.getElementById("speed");
+  let runDate = document.getElementById("date");
+  let runTime = document.getElementById("time");
+  let runObject = {
+    speed: speed.value,
+    date: date.value,
+    time: time.value
+  }
+  fetch('/runInfo', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'},
+    body: JSON.stringify(runObject)
+  })
+  .then(response => response.json())
+  .then(data => loadInfo(data));
+};
