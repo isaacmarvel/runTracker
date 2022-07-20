02b4a61ac38fb2c8d771a288ebc14fa76db3773d
Isaac <isaac.marvel.77@gmail.com>
2022-07-20 11:26:41 -0400

    Got chart.js to display on page

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const sqlite = require('better-sqlite3');
 const app = express();
 const port = 3000;
+// const port = process.env.PORT || 3001; will need these two lines for when I deploy this, apparently
 app.use(express.static('public')); //sets up middleware to send file automatically if it matches file in public directory
 app.use(express.json()); //parses json into an object
 
-function getInfo(db) { //research what these mean
+function getInfo(db) { 
     const stmt = db.prepare("SELECT date, speed, time FROM runInfo");
     const infoFromDb = stmt.all();
     return infoFromDb;
@@ -26,7 +27,8 @@ function initDb() { //creates the database table if it doesn't already exist
 
 initDb();
 
-app.get('/runInfo', (req, res) => {
+app.get('/runInfo/:date', (req, res) => { //Could change to /run/info/:date. ":date" says whatever I pass in will be assigned to date proeprty
+    // req.params.date(maybe not right) would give an object with date property
     let db = sqlite("runInfo.db")
     let runInfoFromDb = getInfo(db);
     res.send(JSON.stringify(runInfoFromDb))
@@ -35,11 +37,16 @@ app.get('/runInfo', (req, res) => {
 app.post('/runInfo', (req, res) => {
     let db = sqlite("runInfo.db")
     const stmt = db.prepare("insert into runInfo (date, speed, time) values (?, ?, ?);");
-    const info = stmt.run(req.body.date, req.body.speed, req.body.time);
+    const info = stmt.run(req.body.date, req.body.speed, req.body.time); //could add as query parameters onto the get side. Example: or, could do run/date and fetch everything from that date
     let runInfoFromDb = getInfo(db);
     res.send(JSON.stringify(runInfoFromDb))
 }); 
 
 app.listen(port, () => {
-    console.log(`app listening on port ${port}`)
+    console.log(` App listening on port ${port}`)
 });
+// api design: instead of /runInfo working like a js variable, have it run instead like a weblink works. /run/info
+
+//would need server also live, so app.js is also live
+//if you connect to a database, gonna need some code changes
+//
--- public/client.js
+++ public/client.js
@@ -1,46 +1,47 @@
 function openMenu() {
-    let x = document.getElementById("myLinks");
-     if (x.style.display === "block") {
-       x.style.display = "none";
-     } else {
-      x.style.display = "block";
-    }
-} 
+      let x = document.getElementById("myLinks");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+  } 
 
-//function validateForm() {
+  //function validateForm() {
 
-//}
+  //}
 
-fetch('/runInfo') 
-  .then(response => response.json())
-  .then(loadInfo);
+  fetch('/runInfo') 
+    .then(response => response.json())
+    .then(loadInfo);
 
-function loadInfo(runInfo) {
-  let infoElements = "";
-  for (let info of runInfo) {
-    infoElements += `<li>${info.date} - ${info.speed} - ${info.time} </li>`
+  function loadInfo(runInfo) {
+    let infoElements = "";
+    for (let info of runInfo) {
+      infoElements += `<li>${info.date} - ${info.speed} - ${info.time} </li>`
+    };
+    let ul = document.getElementById("infoList");
+    ul.innerHTML = infoElements;
+    console.log(infoElements);
   };
-  let ul = document.getElementById("infoList");
-  ul.innerHTML = infoElements;
-  console.log(infoElements);
-};
 
-function sendData() {
-  let runSpeed = document.getElementById("speed");
-  let runDate = document.getElementById("date");
-  let runTime = document.getElementById("time");
-  let runObject = {
-    speed: runSpeed.value,
-    date: runDate.value,
-    time: runTime.value
-  }
+  function sendData() {
+    let runSpeed = document.getElementById("speed");
+    let runDate = document.getElementById("date");
+    let runTime = document.getElementById("time");
+    let runObject = {
+      speed: runSpeed.value,
+      date: runDate.value,
+      time: runTime.value
+    }
 
-  fetch('/runInfo', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'},
-    body: JSON.stringify(runObject)
-  })
-  .then(response => response.json())
-  .then(data => loadInfo(data));
-};
+    fetch('/runInfo', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'},
+      body: JSON.stringify(runObject)
+    })
+    .then(response => response.json())
+    .then(data => loadInfo(data));
+  };
+
